f552dead0d03b9587871fe780b8e3de8ac5b43de
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-18 16:04:09 -0400

    up and running, post request handeler created

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 // implement your API here
-v
+const express = require("express");
+
+const dataBase = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+
+
+
+
+server.post('/api/users', (req, res) => {
+
+    const newUser = req.body;
+
+    if(newUser.name && newUser.bio){ 
+        dataBase.insert(newUser)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(err => res.status(500).json({message: 'error adding new user'}))
+    }else{
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."});
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+server.listen(4000, () => {
+    console.log("\n** API up and  running on port 4k **");
+});
