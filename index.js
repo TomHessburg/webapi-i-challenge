5369f8f1816aadf28c18c665012c5627b38f3e5b
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-18 16:12:40 -0400

    get user by id working

--- index.js
+++ index.js
@@ -33,6 +33,20 @@ server.get('/api/users', (req,res) => {
 
 })
 
+server.get('/api/users/:id', (req,res) => {
+    const id = req.params.id 
+
+    dataBase.findById(id)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user);
+            } else{
+                res.status(404).json({message: "The user with the specified ID does not exist."})
+            }
+        })
+        .catch(err => res.status(500).json({message: "The user information could not be retrieved."}))  
+})
+
 
 
 
